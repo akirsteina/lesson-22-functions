cd842dd31cb55c9d8e793895e9f4697b9b166d7f
akirsteina <akirsteina@users.noreply.github.com>
2021-03-23 21:41:10 +0200

    finish 10th task

--- task-10/js/main.js
+++ task-10/js/main.js
@@ -1,19 +1,18 @@
-function identityMatrix(num){
-    
-for (let i=0; i<=num; i++){
-    let array =[];
-    let row = [];
-    for (let j=0; j<=num; j++){
-        
-        if (i===j){
-            row.push(' 1 ') ;
-        } else {
-            row.push(' 0 ') ;
+function identityMatrix(num) {
+    let array = [];
+    for (let i = 0; i <= num; i++) {
+        let row = [];
+        for (let j = 0; j <= num; j++) {
+
+            if (i === j) {
+                row.push(' 1 ');
+            } else {
+                row.push(' 0 ');
+            }
         }
+        array.push(row);
     }
-    array.push(row);
-}
-console.log(array);
+    console.log(array);
 }
 
 console.log(identityMatrix(3));

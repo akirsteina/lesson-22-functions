8d04f6c12b75828e06e045af50f0a7f7265b83d1
Alisenda <78229206+Alisenda@users.noreply.github.com>
2021-03-23 20:07:45 +0200

    Update main.js
    task5

--- task-5/js/main.js
+++ task-5/js/main.js
@@ -1,3 +1,16 @@
 // 6. Write a function that accepts a string as a parameter and find the longest word within the string.
 // Example string : 'Web Development Tutorial'
 // Expected Output : 'Development'
+
+function uppercase(phrase) {
+    let words = phrase.split(' ');
+    
+    for (i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1);
+    
+    }
+
+return words.join(' ');
+}
+
+console.log(uppercase('the quick brown fox'))

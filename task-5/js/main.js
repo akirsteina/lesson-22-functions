5b6b36a60276d0c8ca4bfcc0abd1906ee4cd1fde
akirsteina <akirsteina@users.noreply.github.com>
2021-03-23 20:06:45 +0200

    add task 5

---
+++ task-5/js/main.js
@@ -0,0 +1,3 @@
+// 6. Write a function that accepts a string as a parameter and find the longest word within the string.
+// Example string : 'Web Development Tutorial'
+// Expected Output : 'Development'
